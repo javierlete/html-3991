9a747f3c26b2983ac8f0446dc7d86fe69f649e0a
Javier Lete <javier@eskura21.com>
2023-03-23 18:38:30 +0100

    JavaScript toma de contacto

---
+++ JAVASCRIPT/js/sintaxis.js
@@ -0,0 +1,24 @@
+/*
+Comentario multilínea
+*/
+
+// Comentario de línea
+
+// alert('Prueba');
+
+// nombre = prompt('Dime tu nombre');
+
+// console.log('Hola ' + nombre);
+
+// console.log(`Hola ${nombre}`); // ES2015
+
+x = +prompt('X');
+y = parseInt(prompt('Y'));
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+
+suma = x + y;
+
+console.log('El resultado de ' + x + ' + ' + y + ' = ' + suma);
+console.log(`El resultado de ${x} + ${y} = ${suma}`);
