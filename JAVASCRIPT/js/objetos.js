1f3f52f22057a4ba41ff5bb8a1b90ffd98eebc5f
Javier Lete <javier@eskura21.com>
2023-03-28 17:38:28 +0200

    Objetos

---
+++ JAVASCRIPT/js/objetos.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const persona = new Object();
+
+persona.nombre = 'Javier';
+persona.apellido = 'Lete';
+
+persona.hobbies = [ 'Música', 'Informática' ];
+
+console.log(persona);
+
+console.log(persona.hobbies[1]);
+
+console.log({ id: 5, nombre: 'Otro ejemplo' });
+
+const persona2 = {
+    nombre: 'Pepe',
+    apellido: 'Pérez',
+    hobbies: [ 'Macramé', 'Ramen' ]
+};
+
+console.log(persona2);
+console.log(persona2.nombre);
+console.log(persona2['apellido']);
+
+const personas = [ persona, persona2, { nombre: 'Juan' }];
+
+personas[1].apellidos = 'Pérez González';
+
+console.log(personas);
