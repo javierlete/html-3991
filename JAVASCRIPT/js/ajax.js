4270b41a0b4ebe12da9f3f5c6b2396908786f135
Javier Lete <javier@eskura21.com>
2023-04-05 17:33:47 +0200

    Ejemplo de peticiones AJAX con fetch y json-server

--- JAVASCRIPT/js/ajax.js
+++ JAVASCRIPT/js/ajax.js
@@ -1,6 +1,37 @@
 window.addEventListener('DOMContentLoaded', async function() {
-    const respuesta = await fetch('http://localhost:3000/clientes/');
+    let respuesta = await fetch('http://localhost:3000/clientes/');
     const clientes = await respuesta.json();
 
     console.log(clientes);
+
+    respuesta = await fetch('http://localhost:3000/clientes/2');
+    const cliente = await respuesta.json();
+
+    console.log(cliente);
+
+    respuesta = await fetch('http://localhost:3000/clientes', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nombre: 'Nuevo', apellido: 'Nuevez' })
+    });
+
+    console.log(respuesta.statusText);
+
+    respuesta = await fetch('http://localhost:3000/clientes/4', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: 4, nombre: 'Modificado', apellido: 'Modificadez' })
+    });
+
+    console.log(respuesta.statusText);
+
+    respuesta = await fetch('http://localhost:3000/clientes/4', {
+        method: 'DELETE'
+    });
+
+    console.log(respuesta.statusText);
 });
