988622f76e1041f794a73d2f3ca0594d9804dd4b
Javier Lete <javier@eskura21.com>
2023-04-26 17:02:39 +0200

    Estrellas para importante

--- EJEMPLO/js/index.js
+++ EJEMPLO/js/index.js
@@ -13,6 +13,12 @@ const ICONO_PLEGADO = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height
 const ICONO_DESPLEGADO = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
                           </svg>`;
+const ESTRELLA_HUECA = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                        </svg>`;
+const ESTRELLA_RELLENA = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>`;
 let ulTareasPendientes, ulTareasCompletadas, iconoCompletadas;
 
 document.addEventListener('DOMContentLoaded', function (event) {
@@ -136,21 +142,30 @@ function crearLi(tarea) {
     const etiqueta = document.createElement(tipoEtiqueta);
     etiqueta.innerHTML = tarea.titulo;
     li.appendChild(etiqueta);
+    
+    const labelImportante = document.createElement('label');
+    const contenidoLabel = document.createElement('span');
+    contenidoLabel.innerHTML = tarea.importante ? ESTRELLA_RELLENA : ESTRELLA_HUECA;
+    labelImportante.appendChild(contenidoLabel);
+
     // Creamos un nuevo checkbox para marcar la tarea como importante
     const checkboxImportante = document.createElement('input');
 
     checkboxImportante.type = 'checkbox';
     checkboxImportante.title = 'Importante';
     checkboxImportante.checked = tarea.importante;
+    checkboxImportante.style.display = 'none';
 
     checkboxImportante.addEventListener('click', importanteClick);
 
-    li.appendChild(checkboxImportante);
+    labelImportante.appendChild(checkboxImportante);
+    li.appendChild(labelImportante);
+
     return li;
 }
 
 async function importanteClick(e) {
-    const li = e.target.parentElement;
+    const li = e.target.parentElement.parentElement;
     const id = li.dataset.id;
 
     const respuesta = await fetch(`${URL}/${id}`, {
@@ -165,6 +180,9 @@ async function importanteClick(e) {
         alert('No se pudo marcar la tarea como importante');
         return;
     }
+
+    const span = li.querySelector('label span');
+    span.innerHTML = this.checked ? ESTRELLA_RELLENA : ESTRELLA_HUECA;
 }
 
 async function finalizarClick() {
