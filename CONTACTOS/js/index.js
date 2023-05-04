e2a30c71c66ac8c77f7e2a269aae547cd7a83a6a
Javier Lete <javier@eskura21.com>
2023-05-04 16:53:08 +0200

    Buscador

--- CONTACTOS/js/index.js
+++ CONTACTOS/js/index.js
@@ -2,16 +2,40 @@
 
 const URL = 'http://localhost:3000/contactos';
 
+let ulContactos;
+let contactos;
+
 window.addEventListener('DOMContentLoaded', async () => {
-    const ulContactos = document.querySelector('#contactos');
+    ulContactos = document.querySelector('#contactos');
+    const inputBusqueda = document.querySelector('form input[type=search]');
+
+    inputBusqueda.addEventListener('keyup', e => {
+        const busqueda = e.target.value;
+
+        const contactosFiltrados = contactos.filter(
+            // Esta c es diferente de la otra porque están en diferentes ámbitos
+            c => {
+                const nombreCompleto = c.nombre + ' ' + c.apellidos;
+                const nombreLimpio = normalizar(nombreCompleto);
+                const busquedaLimpia = normalizar(busqueda);
+
+                return nombreLimpio.includes(busquedaLimpia);
+            }
+        );
+        listarContactos(contactosFiltrados);
+    });
+
     const respuesta = await fetch(URL);
+    contactos = await respuesta.json();
 
-    console.log("respuesta", respuesta);
+    listarContactos(contactos);
+});
 
-    const contactos = await respuesta.json();
+function normalizar(texto) {
+    return texto.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
 
-    console.log("contactos", contactos);
-
+function listarContactos(contactos) {
     ulContactos.innerHTML = '';
 
     for (const c of contactos) {
@@ -19,9 +43,10 @@ window.addEventListener('DOMContentLoaded', async () => {
         li.innerHTML = `
             <span class="text-bg-secondary rounded-circle p-1 px-2">${c.nombre[0]}</span>
             ${c.nombre} ${c.apellidos}`;
-        
+
         li.className = 'list-group-item border-0';
 
         ulContactos.appendChild(li);
     }
-});
+}
+
